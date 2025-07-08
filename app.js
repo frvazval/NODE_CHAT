398f20cd15ab9c4c099e43d8f74cf5e189a76055
Fvazquez <foapgenerico@365.stucom.com>
2025-07-08 19:46:07 +0200

    stucom

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import logger from 'morgan';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
+process.loadEnvFile();
+const PORT = process.env.PORT;
+
+const app = express();
+const server = createServer(app);
+
+
+
+
+app.use(logger('dev'));
+app.use(express.static(process.cwd() + "/client"));
+
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor abierto en http://localhost:${PORT}`);
+});
+
+
