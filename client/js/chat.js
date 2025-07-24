79a3309556cdfc3491eaaea3fb7098a8de88e2e0
Fvazquez <foapgenerico@365.stucom.com>
2025-07-24 18:15:17 +0200

    stucom

---
+++ client/js/chat.js
@@ -0,0 +1,6 @@
+import {io} from 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.8.1/socket.io.esm.min.js';
+
+const formUserName = document.getElementById('formUserName');
+
+let username = "";
+
