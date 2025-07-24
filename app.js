c843db7d218675f2a4918c282efcab9cae9abae3
Fvazquez <foapgenerico@365.stucom.com>
2025-07-24 16:23:58 +0200

    stucom

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ import express from 'express';
 import logger from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
+import { createClient } from '@libsql/client';
+
 
 process.loadEnvFile();
 const PORT = process.env.PORT;
+
+const db = {
+    url : process.env.DB_URL,
+    authToken : process.env.DB_TOKEN
+};
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+    id_message INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT default "anonimo",
+    fecha TIMESTAMP default CURRENT_TIMESTAMP
+    )
+    `);
 
 const app = express();
 const server = createServer(app);
