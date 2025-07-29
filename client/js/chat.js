a595da9f73fbbd1d9504018ddae5aa400428a57f
Fvazquez <foapgenerico@365.stucom.com>
2025-07-29 16:31:28 +0200

    chat

--- client/js/chat.js
+++ client/js/chat.js
@@ -3,6 +3,8 @@ import {io} from 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.8.1/socket.
 const formUserName = document.getElementById('formUserName');
 
 let username = ""; // nombre del usuario actual
+let socket;
 
+let userColors = {}; // guardar los colores de cada usuario
 
 
